814ec1ad1ea53617ef7de11322679ed5792baa2e
orkhanAbdulla <Orkhanaa@code.edu.az>
2022-09-23 08:39:09 +0400

    Task Added

---
+++ PYP-FtontToBack/Models/ProductPhoto.cs
@@ -0,0 +1,10 @@
+﻿namespace PYP_FtontToBack.Models
+{
+    public class ProductPhoto
+    {
+        public Guid Id { get; set; }
+        public Guid ProductId { get; set; }
+        public Product Product { get; set; }=null!;
+        public string Url { get; set; } = null!;
+    }
+}
